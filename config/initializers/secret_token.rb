41535212c56b2830f401e6a91d37f75ca24afc13
Ruben Epong <rubenjohne@gmail.com>
2013-12-16 16:02:03 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oscarsapp::Application.config.secret_key_base = 'd41753d0c57685b5b94a73ad82426015d23853e88af53e3705479d6a6f279ab971231b18f258574ecf3581e9cedd49bd9c63ad69eaba5552763460d94a89d80f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oscarsapp::Application.config.session_store :cookie_store, key: '_oscarsapp_session'
