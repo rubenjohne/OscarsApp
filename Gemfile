62d0378e1b1d55184cfdfcb1e1ba023820d7dbf4
danny.chen@tadashishoji.com <aznlucidx@gmail.com>
2013-12-18 14:10:10 -0800

    New Branch

--- Gemfile
+++ Gemfile
@@ -32,6 +32,14 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+group :test do
+	gem 'sqlite3'
+end
+
+group :development do
+	gem 'sqlite3'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Oscarsapp::Application.routes.draw do
+  resources :participants
+
   get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
