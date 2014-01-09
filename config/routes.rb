81be192d2cd8ee5aacb82f3aa2c885bd423b8c97
danny.chen@tadashishoji.com <aznlucidx@gmail.com>
2014-01-09 14:18:17 -0800

    css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Oscarsapp::Application.routes.draw do
   resources :participants
 
+  get "participants/mobile"
+
+  root 'participants#mobile'
+
   get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
