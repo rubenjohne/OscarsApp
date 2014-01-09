0f46c3e9eb95145072151d023d3c34490a085449
danny.chen@tadashishoji.com <aznlucidx@gmail.com>
2014-01-09 14:21:31 -0800

    test route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Oscarsapp::Application.routes.draw do
   resources :participants
 
+  get "participants/mobile" => 'participants#mobile'
 
   get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
