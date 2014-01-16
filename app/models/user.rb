bb93da8bccd978b720aea63d93338139fc05c6ef
Ruben Epong <rubenjohne@gmail.com>
2014-01-16 13:52:54 -0800

    modified user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  password   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_accessible :name, :email 
+  
+  VALID_EMAIL_REGEX = /\ A[\ w +\-.] +@[ a-z\ d\-.] +\.[ a-z] +\ z/i
+
+  validates :name, presence: true
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114231539) do
+ActiveRecord::Schema.define(version: 20140116213959) do
 
   create_table "participants", force: true do |t|
     t.string   "name"
