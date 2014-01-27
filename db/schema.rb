651318913199ab92e257b4d727fc0648f867fb6c
Ruben Epong <rubenjohne@gmail.com>
2014-01-27 15:57:41 -0800

    removed users data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117235720) do
+ActiveRecord::Schema.define(version: 20140127235307) do
 
   create_table "participants", force: true do |t|
     t.string   "name"
@@ -19,16 +19,5 @@ ActiveRecord::Schema.define(version: 20140117235720) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "password_digest"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
